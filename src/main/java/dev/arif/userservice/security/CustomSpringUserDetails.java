b9261b915863604e7c3273e70005f12066685985
Arif Khan <arif710@gmail.com>
2023-11-14 22:34:03 +0530

    stored the consent and token in db

--- src/main/java/dev/arif/userservice/security/CustomSpringUserDetails.java
+++ src/main/java/dev/arif/userservice/security/CustomSpringUserDetails.java
@@ -1,12 +1,19 @@
 package dev.arif.userservice.security;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
 import dev.arif.userservice.models.User;
+import lombok.Getter;
+import lombok.Setter;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 
+import java.io.Serializable;
 import java.util.Collection;
-
-public class CustomSpringUserDetails implements UserDetails {
+@Setter
+@Getter
+@JsonDeserialize(as = CustomSpringUserDetails.class)
+public class CustomSpringUserDetails implements UserDetails, Serializable {
 
     private User user;
     CustomSpringUserDetails(){}
@@ -20,32 +27,38 @@ public class CustomSpringUserDetails implements UserDetails {
     }
 
     @Override
+    @JsonIgnore
     public String getUsername() {
         return user.getEmail();
     }
 
     @Override
+    @JsonIgnore
     public String getPassword() {
         System.out.println("password is "+user.getPassword());
         return user.getPassword();
     }
 
     @Override
+    @JsonIgnore
     public boolean isAccountNonExpired() {
         return true;
     }
 
     @Override
+    @JsonIgnore
     public boolean isAccountNonLocked() {
         return true;
     }
 
     @Override
+    @JsonIgnore
     public boolean isCredentialsNonExpired() {
         return true;
     }
 
     @Override
+    @JsonIgnore
     public boolean isEnabled() {
 
             return true;
---
+++ src/main/java/dev/arif/userservice/security/repositories/ClientRepository.java
@@ -0,0 +1,12 @@
+package dev.arif.userservice.security.repositories;
+
+import dev.arif.userservice.security.models.Client;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface ClientRepository extends JpaRepository<Client, String> {
+    Optional<Client> findByClientId(String clientId);
+}
